70a2c014aeea8769e1198ad06511337d29a52dee
unknown <cu.18bcs6591@gmail.com>
2023-05-16 22:22:26 +0530

    Day 12

---
+++ Leetcode-30-days-js-challenge/Day-12-2637. Promise Time Limit.js
@@ -0,0 +1,81 @@
+/* Given an asyncronous function fn and a time t in milliseconds, return a new time limited version of the input function.
+
+A time limited function is a function that is identical to the original unless it takes longer than t milliseconds to fullfill. In that case, it will reject with "Time Limit Exceeded".  Note that it should reject with a string, not an Error.
+
+ 
+
+Example 1:
+
+Input: 
+fn = async (n) => { 
+  await new Promise(res => setTimeout(res, 100)); 
+  return n * n; 
+}
+inputs = [5]
+t = 50
+Output: {"rejected":"Time Limit Exceeded","time":50}
+Explanation:
+The provided function is set to resolve after 100ms. However, the time limit is set to 50ms. It rejects at t=50ms because the time limit was reached.
+Example 2:
+
+Input: 
+fn = async (n) => { 
+  await new Promise(res => setTimeout(res, 100)); 
+  return n * n; 
+}
+inputs = [5]
+t = 150
+Output: {"resolved":25,"time":100}
+Explanation:
+The function resolved 5 * 5 = 25 at t=100ms. The time limit is never reached.
+ */
+
+// Solution One - Using Callback
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+  return async function (...args) {
+    return new Promise((resolve, reject) => {
+      const id = setTimeout(() => reject("Time Limit Exceeded"), t);
+      fn(...args)
+        .then((res) => resolve(res))
+        .catch((err) => reject(err))
+        .finally(() => clearTimeout(id));
+    });
+  };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
+
+//Solution Two - Using async/await
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+  return async function (...args) {
+    return new Promise(async (resolve, reject) => {
+      const id = setTimeout(() => reject("Time Limit Exceeded"), t);
+      try {
+        const res = await fn(...args);
+        resolve(res);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
