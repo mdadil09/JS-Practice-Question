af7440f9fed8d008a8107dd67e822ccc75722f60
Md Adil <90206317+mdadil09@users.noreply.github.com>
2023-06-28 18:33:47 +0530

    Delete DSA directory

--- DSA/2745. Construct the Longest New String.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int longestString(int x, int y, int z) {
-        int mini = min(x,y);
-        if(x == y) return 4*x+2*z;
-        return (2*mini + 2*(mini+1) + 2 * z);
-    }
-};
